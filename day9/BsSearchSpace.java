769d21754f97a3ef605706b820e32069e24e21b7
Chiraggg-p <ckofficial101@gmail.com>
2024-08-10 11:36:56 +0530

    final commit

---
+++ day9/BsSearchSpace.java
@@ -0,0 +1,25 @@
+public class BsSearchSpace {
+
+    static boolean check(int speed , int dist , int maxTime){
+        int distance_travelled = speed*maxTime;
+        return(distance_travelled>=dist);
+    }
+    static int bsSearchSp(int dis , int time ){
+        int low =0,high =1000, mid=0,ans=0;
+        while(low<=high){
+            mid= high-(high-low)/2;
+            if(check(mid, dis, time)){
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid +1;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int dis = 1000;
+        int time = 10;
+        System.out.println(bsSearchSp(dis, time));
+    
+  }
+}
