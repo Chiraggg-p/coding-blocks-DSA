463e45f3c1342e5c2beaec84767f8d6e6f5a6fdd
Chiraggg-p <ckofficial101@gmail.com>
2024-08-25 10:09:21 +0530

    final commit

---
+++ day11/strBld.java
@@ -0,0 +1,20 @@
+public class strBld {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(10);
+        sb.append("hola");
+        sb.append("amigo");
+        sb.replace(0, 0, null)
+        
+        // System.out.println(sb.substring(5 , 6));
+        // System.out.println(sb.insert(4," "));
+        // sb.reverse();
+        System.out.println(sb);
+        // sb.ensureCapacity(100);
+        System.out.println(sb.capacity());
+        // System.out.println(sb.length());
+
+       
+
+    }
+
+}
